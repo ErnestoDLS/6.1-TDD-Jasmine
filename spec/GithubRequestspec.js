ba180188c6b918539c7eee2a5934db3ab41867d7
Ernesto De Los Santos <netos_id@yahoo.com>
2016-05-16 23:18:37 -0500

    no failures

--- spec/GithubRequestspec.js
+++ spec/GithubRequestspec.js
@@ -13,28 +13,44 @@ describe("Github Request Spec", function(){
 
   it("Parses name", function(){
     githubRequest.getData();
-    expect(githubRequest.returnObject.name).toEqual("Ernesto De Los Santos");
+    setTimeout(function(){
+      expect(githubRequest.returnObject.name).toEqual("Ernesto De Los Santos");
+    },5000);
   });
 
-  // it("Parses url", function(){
-  //   expect(githubRequest.returnObject.url).toEqual("https://api.github.com/users/ErnestoDLS");
-  // });
-  //
-  // it("Parses repos", function(){
-  //   expect(githubRequest.returnObject.public_repos).toEqual(19);
-  // });
-  //
-  // it("Parses login", function(){
-  //   expect(githubRequest.returnObject.login).toEqual("ErnestoDLS");
-  // });
-  //
-  // it("Parses avatar url", function(){
-  //   expect(githubRequest.returnObject.avatar_url).toEqual("https://avatars.githubusercontent.com/u/17814442?v=3");
-  // });
-  //
-  // it("Requests data from URL", function(){
-  //   spyOn(githubRequest, "getData");
-  //   githubRequest.getData("https://api.github.com/users/ernestodls");
-  //   expect(githubRequest.getData).toHaveBeenCalledWith("https://api.github.com/users/ernestodls");
-  // });
+  it("Parses url", function(){
+    githubRequest.getData()
+    setTimeout(function(){
+      expect(githubRequest.returnObject.url).toEqual("https://api.github.com/users/ErnestoDLS");
+    },5000);
+  });
+
+  it("Parses repos", function(){
+    githubRequest.getData()
+    setTimeout(function(){
+      expect(githubRequest.returnObject.public_repos).toEqual(19);
+    },5000);
+  });
+
+  it("Parses login", function(){
+    githubRequest.getData();
+    setTimeout(function(){
+      expect(githubRequest.returnObject.login).toEqual("ErnestoDLS");
+
+    },5000);
+  });
+
+  it("Parses avatar url", function(){
+    githubRequest.getData()
+    setTimeout(function(){
+      expect(githubRequest.returnObject.avatar_url).toEqual("https://avatars.githubusercontent.com/u/17814442?v=3");
+
+    },5000);
+  });
+
+  it("Requests data from URL", function(){
+    spyOn(githubRequest, "getData");
+    githubRequest.getData("https://api.github.com/users/ernestodls");
+    expect(githubRequest.getData).toHaveBeenCalledWith("https://api.github.com/users/ernestodls");
+  });
 });
